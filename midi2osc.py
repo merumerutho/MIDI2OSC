94ae692532ddb726cb30c1d51344650d0aa682c2
merumerutho <merutochan@gmail.com>
2022-02-16 20:31:57 +0100

    Added OSC Message forwarding mechanism!

--- midi2osc.py
+++ midi2osc.py
@@ -1,18 +1,20 @@
 import defs
 import logging
+import configuration_interface as IConfig
+
 
 def eval_midi_msg(config, msg, client):
-    known_midi = False
+    result = False
     if msg:
         delta = msg[1]
         msg = msg[0]
         # Now, parse message through the currently supported methods
-        known_midi = check_cc(config, msg, client) or\
-                     check_note(config, msg, client) or\
-                     check_start_continue(config, msg, client) or\
+        result = check_cc(config, msg, client) or \
+                     check_note(config, msg, client) or \
+                     check_start_continue(config, msg, client) or \
                      check_stop(config, msg, client)
 
-        if not known_midi:
+        if not result:
             logging.info(msg)
     return
 
@@ -22,7 +24,11 @@ def check_cc(config, msg, client):
         cmd, ctrl, data = msg[0], msg[1], msg[2]
         channel = (cmd - defs.CC_START_RANGE)
         logging.info("Midi CC: channel {}, control {}, data {}".format(channel, ctrl, data))
-
+        dest = IConfig.get_dest_cc(config, channel, ctrl)
+        if dest == defs.ERR_DEST_NOT_FOUND:
+            return False
+        # Send message
+        client.send_message(dest, data)
         return True
 
 
@@ -30,24 +36,40 @@ def check_note(config, msg, client):
     if defs.NOTE_ON_START_RANGE <= msg[0] <= defs.NOTE_ON_END_RANGE:
         channel, note, velocity = (msg[0] - defs.NOTE_ON_START_RANGE), msg[1], msg[2]
         logging.info("Midi note on: channel {}, note {}, velocity {}".format(channel, note, velocity))
-
+        dest = IConfig.get_dest_note_on(config, channel)
+        if dest == defs.ERR_DEST_NOT_FOUND:
+            return False
+        # Send message
+        client.send_message(dest, [note, velocity])
         return True
     elif defs.NOTE_OFF_START_RANGE <= msg[0] <= defs.NOTE_OFF_END_RANGE:
         channel, note, velocity = (msg[0] - defs.NOTE_OFF_START_RANGE), msg[1], msg[2]
         logging.info("Midi note off: channel {}, note {}, velocity {}".format(channel, note, velocity))
-
+        dest = IConfig.get_dest_note_off(config, channel)
+        if dest == defs.ERR_DEST_NOT_FOUND:
+            return False
+        # Send message
+        client.send_message(dest, [note, velocity])
         return True
 
 
 def check_start_continue(config, msg, client):
     if defs.MIDI_START == msg[0] or defs.MIDI_CONTINUE == msg[0]:
         logging.info("Midi Start/Continue")
-
+        dest = IConfig.get_dest_start(config)
+        if dest == defs.ERR_DEST_NOT_FOUND:
+            return False
+        # Send message
+        client.send_message(dest, 1)
         return True
 
 
 def check_stop(config, msg, client):
     if defs.MIDI_STOP == msg[0]:
         logging.info("Midi Stop")
-
+        dest = IConfig.get_dest_stop(config)
+        if dest == defs.ERR_DEST_NOT_FOUND:
+            return False
+        # Send message
+        client.send_message(dest, 1)
         return True
